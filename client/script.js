b3e7b7eca8391cd7aa25d2767554e48500613911
Jasurbek <jasurisoqov685@gmail.com>
2023-01-20 11:34:59 +0500

    Creating Chat Stripe function

--- client/script.js
+++ client/script.js
@@ -42,3 +42,21 @@ function generateUniqueId() {
 
   return `id-${timeStamp}-${hexaDecimalString}`;
 }
+
+// Chat Stripe function
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `;
+}
