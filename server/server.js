cc02d25c524e22594c6bdb3393d307fdecc8cfeb
Jasurbek <jasurisoqov685@gmail.com>
2023-01-23 08:55:35 +0500

    Open AI app pest requset and response

--- server/server.js
+++ server/server.js
@@ -20,3 +20,25 @@ app.get("/", async (req, res) => {
     message: "Hello World",
   });
 });
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "code-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
